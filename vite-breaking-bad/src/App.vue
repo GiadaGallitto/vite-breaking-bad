037c12147fffc7e81ba2cf8beca715fa5488743a
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-11 15:44:36 +0100

    Create header elements

--- vite-breaking-bad/src/App.vue
+++ vite-breaking-bad/src/App.vue
@@ -1,12 +1,32 @@
-<script >
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
+export default {
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
+  <header>
+    <AppHeader />
+  </header>
 
+  <main>
+    <AppMain />
+  </main>
 
+  <footer>
+    <AppFooter />
+  </footer>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
--- vite-breaking-bad/src/components/AppHeader.vue
+++ vite-breaking-bad/src/components/AppHeader.vue
@@ -1,11 +1,23 @@
-<script >
+<script>
 
 </script>
 
 <template>
-
+  <div class="header-container">
+    <div class="logo"></div>
+    <h1>Yu-Gi-Oh Api</h1>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.header-container{
+  height: 70px;
+  background-color: white;
+  display: flex;
+  align-items: center;
 
+  h1{
+    font-weight: 400;
+  }
+}
 </style>
