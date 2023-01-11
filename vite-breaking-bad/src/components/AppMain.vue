5fc9f7361a9d9d1337d963627b9f270707534843
GiadaGallitto <gallitto.giada@gmail.com>
2023-01-11 15:53:00 +0100

    Create found section of main

--- vite-breaking-bad/src/components/AppMain.vue
+++ vite-breaking-bad/src/components/AppMain.vue
@@ -1,10 +1,28 @@
-<script >
+<script>
 
 </script>
 
 <template>
-
+  <div class="main-container">
+    <section class="cards-found">
+      <h4>Found ...</h4>
+    </section>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+  .main-container{
+    width: 90%;
+    height: 1000px;
+    margin: 5rem auto;
+    background-color: white;
+    // border-radius: 20px;
+    padding: 3.5rem;
+    .cards-found{
+      width: 100%;
+      background-color: #212529;
+      padding: 1.5rem 1rem;
+      color: white;
+    }
+  }
 </style>
